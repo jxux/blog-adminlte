aac1a219074fd13af2a78483b95d5b91d1d35f96
= <=>
2021-03-16 15:13:54 -0500

    roles y seeders

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('adminlte::page')
+
+@section('title', 'Usuarios')
+
+@section('content_header')
+    <h1>Asignar un rol</h1>
+@stop
+
+@section('content')
+
+    @if (session('info'))
+        <div class="alert alert-success">
+            <strong> {{ session('info') }}</strong>
+        </div>
+    @endif
+    <div class="card">
+        <div class="card-body">
+            <p class="h5">Nombre</p>
+            <p class="form-control">{{ $user->name }}</p>
+
+            {!! Form::model($user, ['route' => ['admin.users.update', $user], 'method' => 'put']) !!}
+                @foreach ($roles as $role)
+                    <div>
+                        <label>
+                            {!! form::checkbox('roles[]', $role->id, null, ['class' => 'mr-1']) !!}
+                            {{$role->name}}
+                        </label>
+                    </div>
+                @endforeach
+                {!! Form::submit('Actualizar roles', ['class' => 'btn btn-primary mt-2']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+@stop
+
+@section('css')
+    
+@stop
+
+@section('js')
+    
+@stop
--- routes/admin.php
+++ routes/admin.php
@@ -12,6 +12,6 @@ Route::get('', [AdminController::class, 'index'])->name('admin.index');
 Route::resource('categories', CategoryController::class)->names('admin.categories');
 Route::resource('tags', TagController::class)->names('admin.tags');
 Route::resource('posts', PostController::class)->names('admin.posts');
-Route::resource('users', UserController::class)->names('admin.users');
+Route::resource('users', UserController::class)->only('index', 'edit', 'update')->names('admin.users');
 
  
